88e480d6e79c66e382024db424d21afb05fdd7f6
Jaydie <johndenieldomalaon@gmail.com>
2025-03-29 03:27:54 +0800

    I had a problem about the output. At first I did not know what the problem was, I saw that in line 5 each '' does not have a space so i added space to get the correct output

--- implementing_center()_function.py
+++ implementing_center()_function.py
@@ -1,7 +1,9 @@
 def implement_center(s, width):
-    spaces = max(widrh - len(s), 0)
+    spaces = max(width - len(s), 0)
     lspaces = spaces // 2
     rspaces = spaces - lspaces
-    return ''*lspaces + s + ''*rspaces
+    return ' '*lspaces + s + ' '*rspaces
+
+print(implement_center("Hello", 10))
 
 
