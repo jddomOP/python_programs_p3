d847e8d6573e05ff2b17ad775caa644f40997a6b
Jaydie <johndenieldomalaon@gmail.com>
2025-03-29 03:50:38 +0800

    well there is still wrong with the code. Added space on line 4 specifically in ('')

--- implementing_title()_function.py
+++ implementing_title()_function.py
@@ -1,7 +1,8 @@
 def implement_title(s):
     words = s.split()
     result = [word[0].upper() + word[1:].lower() if word else '' for word in words]
-    return ''.join(result)
+    return ' '.join(result)
 
+#test
 print(implement_title("hello world im jaydie"))
 
