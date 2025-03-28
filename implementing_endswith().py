e0c2f63fd7205ab98d0bf0cbe43f62ff33819080
Jaydie <johndenieldomalaon@gmail.com>
2025-03-29 03:12:43 +0800

    tested if it print true and it does. I also based on the given problem codes

--- implementing_endswith().py
+++ implementing_endswith().py
@@ -1,4 +1,4 @@
 def implement_endswith(s , suffix):
-    return s[-len(suffix):] == suffix if len(suffix) <= len(s) else
-False
+    return s[-len(suffix):] == suffix if len(suffix) <= len(s) else False
 
+print(implement_endswith("HelloWorld", "World"))
