9c89d56fd0eb58fcb2793b398f2e884abef522ea
Jaydie <johndenieldomalaon@gmail.com>
2025-03-29 02:49:30 +0800

    for this part, i asked chatgpt what to do since I don't have a clue on what to do here

--- implementing_lower().py
+++ implementing_lower().py
@@ -1,1 +1,9 @@
 def implement_lower(s):
+    result = ''
+    for char in s:
+        if 'A' <= char <= 'Z':
+            result += chr(ord(char) +  32)
+        else:
+            result += char
+        return result
+
