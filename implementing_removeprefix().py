c1765c212c3394eb39e117fdb264d8d8bccb3fc8
Jaydie <johndenieldomalaon@gmail.com>
2025-03-29 02:43:02 +0800

    well i have issues, code is not running after and i'm figuring out what is the problem

--- implementing_removeprefix().py
+++ implementing_removeprefix().py
@@ -1,5 +1,7 @@
 def implement_removeprefix(s, prefix):
     if s.startwith(prefix):
-        return s[len(prefix):]
+        return s[len(prefix): ]
     return s
 
+#Test
+print(implement_removeprefix("hello world", "hello"))
