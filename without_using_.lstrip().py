44f8d540e98006e277f4c25e385beee7ea297d20
Jaydie <johndenieldomalaon@gmail.com>
2025-03-29 02:36:30 +0800

    used while loop (honestly I still don't get this)

--- without_using_.lstrip().py
+++ without_using_.lstrip().py
@@ -1,7 +1,10 @@
 #before i start, I searched the ff. codes and are still in the process of learning it
 
-
-
-
-
 def custom_lstrip(s):
+    word = 0
+    while word < len(s) and s[word] == ' ':
+        word += 1
+    return s[word: ]
+
+print(custom_lstrip("       hello world"))
+
