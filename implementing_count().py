51a7deb222316b9adcdfdc16c1eab4d20f89fc7a
Jaydie <johndenieldomalaon@gmail.com>
2025-04-01 23:35:47 +0800

    Got confused on the indention since it showed a lot of errors. forgot to put 's' on the if-statement that's why i got tons of error warnings. added a sample for the program.

--- implementing_count().py
+++ implementing_count().py
@@ -1,3 +1,9 @@
-def implement_count(s):
+def implement_count(s, sub):
     count = 0
-    for i in range ()
+    for i in range(len(s) - len(sub) + 1):
+        if s[i:i+len(sub)] == sub:
+            count += 1
+    return count
+
+#sample
+print(implement_count("Hello", 'o'))
