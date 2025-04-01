c0daed07f7197b65d9f974432d70c4b9f2eb4456
Jaydie <johndenieldomalaon@gmail.com>
2025-04-01 22:42:47 +0800

    having problems with my code, i think there is wrong with line 3 due to my improper placement of "len(s)" function

--- implement_removesuffix.py
+++ implement_removesuffix.py
@@ -1,4 +1,6 @@
-def implement_removesuffix(s):
+def implement_removesuffix(s, suffix):
     if s.endswith(suffix):
         return s[len(s): ]
     return s
+
+print(implement_removesuffix("Hello world", "world"))
