b42256135b48e2838ca7284572c68c9bb069def7
Jaydie <johndenieldomalaon@gmail.com>
2025-04-01 23:53:40 +0800

    Got a blank output when i first tried, saw that the "print()" was not in the correct indent. Also added line 5 if the sub is not found. Still confuse on the code but still studying this given problem or code

--- implementing_rindex().py
+++ implementing_rindex().py
@@ -1,6 +1,10 @@
-def implement(s, sub):
+def implement_rindex(s, sub):
     for i in range(len(s) - len(sub), -1, -1):
         if s[i:i+len(sub)] == sub:
             return i
+        raise ValueError("substring not found")
+
+#sample
+print(implement_rindex("banana", "ana"))
 
 
