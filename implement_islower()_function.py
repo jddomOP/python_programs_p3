17ad53a74e6039e79965557b02b7599b1c4a888b
Jaydie <johndenieldomalaon@gmail.com>
2025-04-01 22:59:25 +0800

    code works but i dont get any output. checking for possible incorrect input

--- implement_islower()_function.py
+++ implement_islower()_function.py
@@ -2,3 +2,8 @@ def implement_islower(s):
     for char in s:
         while 'a' <= char <= 'z':
                 return True
+        return False
+
+    #Test
+    print(implement_islower("HELLO"))
+    print(implement_islower("hello"))
